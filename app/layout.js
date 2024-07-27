32504068f425b234a17e9312fb0c9ae2df72100c
thisSurya123 <thisurya31@gmail.com>
2024-07-27 19:46:47 +0700

    commit changes

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {Nav, Footer} from "./component";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
 'use client'
 import { CardH, Jumbotron, Product } from "./component";
 
+
 export default function Home() {
   return (
     <>
-    <Jumbotron />
+    <Jumbotron/>
     <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
     <CardH />
     <hr id="product" class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
     <h1  className="md:text-5xl text-2xl text-[#973131] text-center font-bold md:my-5">Get Now!</h1>
     <div className="flex flex-wrap justify-center my-10 md:flex-row flex-col">
-    <Product pic={'product.jpg'}/>
-    <Product pic={'songket2.jpg'}/>
-    <Product pic={'product.jpg'}/>
-    <Product pic={'songket2.jpg'}/>
+    <Product pic={'img/product.jpg'}/>
+    <Product pic={'img/songket2.jpg'}/>
+    <Product pic={'img/white.jpg'}/>
+    <Product pic={'img/songket-white.jpg'}/>
     </div>
     <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
     </>
